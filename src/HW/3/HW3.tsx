ff90dcc396a752385845b967be70e34943d4d79f
DmitriyGulevich <kaskodim@gmail.com>
2024-11-21 18:08:21 +0300

    HW3-commit2

--- src/HW/3/HW3.tsx
+++ src/HW/3/HW3.tsx
@@ -24,6 +24,7 @@ export const HW3 = () => {
         // ЗАСЕТАТЬ БЫ ТЕКСТ В texts И НЕ ПОТЕРЯТЬ НАПУТСТВИЕ ИЗ ПРОШЛОГО ВЕКА)
         // А ЗАТЕМ УБРАТЬ ЗА СОБОЙ В currentText
         setTexts([...texts, t]);
+        setCurrentText('');
     };
 
     return (
